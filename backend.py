9292b433498955344dbd688938b3f07a0a9001b3
atharvajoshi <atharvaj77@gmail.com>
2022-01-24 18:30:38 +0530

    Added Documentation

--- backend.py
+++ backend.py
@@ -19,21 +19,23 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html
 def prediction(grey_img_path, model, out_img_path, greyscale_path):
+    # Conversion of RGB to resized Greyscale
     img_arr = cv2.imread(grey_img_path)
     resized = cv2.resize(img_arr, (256, 256))
     img_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
+    # Normalization
     img_gray = (img_gray - 127.5) / 127.5
     img_gray = np.expand_dims(img_gray, -1)
     final = np.expand_dims(img_gray, 0)
 
     predicted_ab = model.predict(final)
-
     squeezed = np.squeeze(predicted_ab)
 
     img_gray = np.float32(img_gray)
     predicted_lab = cv2.merge((img_gray, squeezed))
 
+    # Rescaling LAB Channels
     pred_l = (predicted_lab[:, :, 0] + 1.) * 50.
     pred_ab = (predicted_lab[:, :, 1:] * 127.5)
     pred_l = np.float32(pred_l)
@@ -42,16 +44,17 @@ def prediction(grey_img_path, model, out_img_path, greyscale_path):
     pred_lab = cv2.merge((pred_l, pred_ab))
     pred_lab = np.float32(pred_lab)
 
+    # Converting LAB to BGR
     pred_rgb = cv2.cvtColor(pred_lab, cv2.COLOR_LAB2BGR)
 
     pred_rgb_final = pred_rgb * 255
     final_pred_rgb = pred_rgb_final.astype(int)
 
     final_gray_out = (img_gray * 127.5) + 127.5
+
+    # Writing Processed images to corresponding paths
     cv2.imwrite(greyscale_path, final_gray_out)
     cv2.imwrite(out_img_path, final_pred_rgb)
-
-    print('Done!')
 
 
 UPLOAD_FOLDER = './upload'
@@ -69,6 +72,7 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
+    # Clearing the uploads directory
     if os.listdir('static/uploads'):
         for f in os.listdir('static/uploads'):
             os.remove('static/uploads/' + f)
@@ -76,6 +80,7 @@ def upload():
     form = UploadForm()
 
     if form.validate_on_submit():
+        # Checking if form data is NoneType
         if form.file.data:
             if allowed_file(form.file.data.filename):
                 filename = secure_filename(form.file.data.filename)
